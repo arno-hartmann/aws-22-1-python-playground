2aca2bc71ea29d6e2e1ab71681b8b3a2ddf5b680
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-01 17:07:38 +0100

    second commit

---
+++ list.py
@@ -0,0 +1,39 @@
+from lib2to3.pgen2.token import NEWLINE
+
+
+myFruitList = ["apple","banana","cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+
+myFruitList[2]= "orange"
+
+print(myFruitList)
+
+
+print(" ------------ ")
+
+
+myFinalAnswerTuple = ("apple", "banana", "pinepple",)
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+print(" ------------ ", "___________")
+
+myFavoriteFruitDicitionary = {
+    "Akua" : "apple",
+    "Saanvi" : "banana",
+    "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDicitionary)
+print(type(myFavoriteFruitDicitionary))
+
+print(myFavoriteFruitDicitionary["Akua"])
+print(myFavoriteFruitDicitionary["Saanvi"])
+print(myFavoriteFruitDicitionary["Paulo"])
---
+++ string.py
@@ -0,0 +1,20 @@
+myString = "This is a string."
+print(myString)
+print(type(myString))
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+
+name = input("What is your name? ")
+
+print(name)
+
+color = input("What is your favourite color? ")
+
+animal = input("What is your favourite animal? ")
+
+print("{}, you lika a {} {}". format(name,color,animal))
+
