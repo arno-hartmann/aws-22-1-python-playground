0b0a6f32e6309d5c31993a2c6ad343c1819c0f38
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-02 15:02:48 +0100

    Init Lab15 OS

---
+++ Lab15/lab15.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+
+os.system("ls")
+
+subprocess.run(["ls"])
+
+# subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None)
+
+subprocess.run(["ls","-l"])
+
+subprocess.run(["ls","-l", "README.md"])
+
+
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+
+
+command="ps"
+commandArugement="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command, commandArgument])
